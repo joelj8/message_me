674bbd5c8a78cdd87569747cd981cef123945625
Joel Estrella <joel.j8@gmail.com>
2018-12-15 18:07:03 +0000

    Start authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+helper_method :current_user, :logged_in?
+  
+  def current_user
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound => e
+      @current_user = nil
+      session[:user_id] = nil
+      flash[:danger] = "Session ended"
+      redirect_to root_path
+    end
+  end
+  
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perfom that action"
+      redirect_to root_path
+    end
+  end
+
 end
